edf27b0137080044d93a6e59b041b1eb830326b7
abdiamn08 <abdiamn08@gmail.com>
2023-05-10 21:37:26 +0800

    first commit

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Posts - SantriKoding.com</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body style="background: lightgray">
@@ -25,7 +24,6 @@
                               @forelse ($kategori as $kategori)
                                 <tr>
                                     <td>{{ $kategori->category }}</td>
-                                    <!-- <td>{{ $post->title }}</td> -->
                                 </tr>
                               @endforelse
                             </tbody>
